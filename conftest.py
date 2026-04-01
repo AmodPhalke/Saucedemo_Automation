71a410faab8b3aca71e2f70824827c002d2a578f
AmodPhalke <ashwiniamod.phalke@gmail.com>
2026-04-01 12:55:07 +0530

    fix: use headless mode in CI environment

--- conftest.py
+++ conftest.py
@@ -3,11 +3,16 @@ from playwright.sync_api import sync_playwright
 from pages.login_page import LoginPage
 from pages.cart_page import CartPage
 from config import constants
+import os
 
 @pytest.fixture
 def page():
+    is_ci = os.getenv("CI") == "true"  # GitHub Actions sets this automatically
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False, slow_mo=800)
+        browser = p.chromium.launch(
+            headless=is_ci,  # True in CI, False on your local machine
+            slow_mo=0 if is_ci else 800  # No slow-mo in CI, slow-mo locally
+        )
         page = browser.new_page()
         yield page
         browser.close()
